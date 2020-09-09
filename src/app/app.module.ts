40f03305a3d90599ed3d93ed7629052d841a3fac
Rafal Brzoska <rbrzoska@future-processing.com>
2020-09-09 11:21:13 +0200

    encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule  } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { TestComponent } from './test/test.component';
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'cn-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class TestComponent implements OnInit {
 
