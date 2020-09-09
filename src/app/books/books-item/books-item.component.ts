58df887aa1995c183bbea7c6eb9babfb682dfd00
Rafal Brzoska <rbrzoska@future-processing.com>
2020-09-09 14:33:13 +0200

    Books list

---
+++ src/app/books/books-item/books-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.interface';
+
+@Component({
+  selector: 'cn-books-item',
+  templateUrl: './books-item.component.html',
+  styleUrls: ['./books-item.component.scss']
+})
+export class BooksItemComponent implements OnInit {
+
+  @Input() book: Book;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BooksDetailsComponent } from './books-details/books-details.component';
+import { BooksItemComponent } from './books-item/books-item.component';
 
 
 @NgModule({
-  declarations: [BooksListComponent, BooksDetailsComponent],
+  declarations: [BooksListComponent, BooksDetailsComponent, BooksItemComponent],
   imports: [
     CommonModule,
     BooksRoutingModule
