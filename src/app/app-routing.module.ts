2a00f7a6e876361580f5347f84efe0a8d3e3b17e
Rafal Brzoska <rbrzoska@future-processing.com>
2020-09-09 11:44:43 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
-  { path: 'test', component: TestComponent }
+  { path: '', component: HomeComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
